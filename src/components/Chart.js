b9a694f088b01bd294bacca23eff6722c1cd7053
Harold Jean <haroldjean38@yahoo.com>
2023-01-31 14:53:01 -0500

    added Victory Chart with example data

---
+++ src/components/Chart.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+// import * as V from 'victory';
+import { VictoryBar, VictoryChart } from 'victory';
+
+export default function Chart() {
+  const data = [
+    { quarter: 1, earnings: 13000 },
+    { quarter: 2, earnings: 16500 },
+    { quarter: 3, earnings: 14250 },
+    { quarter: 4, earnings: 19000 },
+  ];
+  return (
+    <VictoryChart>
+      <VictoryBar
+        data={data}
+        // data accessor for x values
+        x="quarter"
+        // data accessor for y values
+        y="earnings"
+      />
+    </VictoryChart>
+  );
+}
